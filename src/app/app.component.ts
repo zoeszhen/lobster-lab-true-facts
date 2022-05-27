4ef6684c385bfaa35e4860fb619a5c0082a8e982
SHI ZHEN <shi.zhen@vincit.fi>
2022-05-27 21:17:07 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+const FACTS_URL = 'https://non-ssr-angular.firebaseio.com/facts.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-true-facts';
+  facts: Observable<any>;
+  constructor(private http: HttpClient) {
+    this.facts = this.http.get(FACTS_URL);
+  }
 }
